d3df0d1c5691f7d825ab13863d848f381329b578
giubidev <135030402+giubidev@users.noreply.github.com>
2023-05-31 09:06:52 -0300

    Update aula 01.py

--- Tipos Primitivos/aula 01.py
+++ Tipos Primitivos/aula 01.py
@@ -1,3 +1,19 @@
+# Pedindo para o usuário digital 
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite mais um número: '))
 
+# Soma dos dois valores digitados
+s = n1 + n2
 
+# Mostrar para o usuário a soma
+print('A soma vale', s)
+
+# Print com mais recursos
+print('A soma vale {}'.format(s))
+
+# Tipos primitivos:
+# int = número inteiro (ex: 7, -4, 0, 92839)
+# float = número com ponto (ex: 4.5, 0.076, 0.99999)
+# bool = boleanos (ex: True e False) - a primeira letra maiuscula
+# str = strings (ex: 'escrita', 'ola', '8')
 
