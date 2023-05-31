11aa310bdecc8c636c7649e6f96a19446be28b20
giubidev <135030402+giubidev@users.noreply.github.com>
2023-05-31 10:10:20 -0300

    Update pratica1.py

--- Tipos Primitivos/pratica1.py
+++ Tipos Primitivos/pratica1.py
@@ -1,2 +1,12 @@
 # Crie um programa que leia dois números e mostre a soma entre eles
 
+# Solicitando ao usuário os números 
+numero1 = int(input("Digite o primeiro número"))
+numero2 = int(input("Digite o segundo númrero"))
+
+# Fazendo a soma dos números solicitados
+soma = numero1 + numero2
+
+# Exibindo na tela o resultado
+print("A soma entre o número {} e o {}, vale {}".format(numero1, numero2, soma))
+
