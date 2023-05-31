89cd95e45d2890e53296fdb498de780286c47dd8
Giulia Bazan <135030402+giubidev@users.noreply.github.com>
2023-05-31 10:59:53 -0300

    Create pratica5.py

---
+++ Operadores Aritméticos/pratica5.py
@@ -0,0 +1,22 @@
+# Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada
+
+# Solicitando o número para o usuário
+n = int(input("Digite um número inteiro qualquer: "))
+
+# Calculando a tabuada do número 
+tab1 = (n * 1)
+tab2 = (n * 2)
+tab3 = (n * 3)
+tab4 = (n * 4)
+tab5 = (n * 5)
+tab6 = (n * 6)
+tab7 = (n * 7)
+tab8 = (n * 8)
+tab9 = (n * 9)
+tab10 = (n * 10)
+
+# Exibindo na tela a tabuada 
+print("A tabuada do número {}, é {}, {}, {}, {}, {}, {}, {}, {}, {}, {}".format(n,tab1, tab2, tab3, tab4, tab5,
+                                                                                tab6, tab7, tab8,
+                                                                                tab9, tab10))
+
